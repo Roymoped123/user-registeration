94f4606cc714a33876d957539bb3ecee7eaa7b56
Roymoped123 <Roymoped123>
2023-01-06 21:01:35 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -3,9 +3,18 @@ from django.http import HttpResponse
 # Create your views here.
 from app.forms import *
 from django.core.mail import send_mail
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
     return render(request,'home.html')
+
 
 def registration(request):
     uf=UserForm()
@@ -23,6 +32,7 @@ def registration(request):
             PFO=PD.save(commit=False)
             PFO.user=USO
             PFO.save()
+
             send_mail('User registration',
             'Regiistration is success full',
             'akulanaresh901@gmail.com',
@@ -32,3 +42,21 @@ def registration(request):
 
             return HttpResponse('Registration is successful')
     return render(request,'registration.html',d)
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(username=username,password=password)
+        if user and user.is_active:
+            login(request,user)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+
+
+    return render(request,'user_login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
